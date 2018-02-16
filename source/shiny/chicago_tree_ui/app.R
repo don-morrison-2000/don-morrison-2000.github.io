00a71de35286acb4c12b68de8e1f555e65d8b5c4
don-morrison-2000 <35644326+don-morrison-2000@users.noreply.github.com>
2018-02-16 10:43:14 -0600

    Add files via upload
    added AIC stat, added code so one version funs locally and remote

--- source/shiny/chicago_tree_ui/app.R
+++ source/shiny/chicago_tree_ui/app.R
@@ -8,26 +8,38 @@
 #
 
 # source('D:/CRTI/r_projects/shinyapp/chicago_tree_ui/app.R')
-# runApp('app.R')
+# runApp('chicago_tree_ui')
 
 # Load packages
 library(shiny)
 library(shinythemes)
 library(ggplot2)
 
-run_local=FALSE
+chicago_tree_app_local_file <- 'D:/CRTI/r_projects/chicago_tree_dam/chicagotree_dam.r'
+chicago_tree_app_url <- 'https://don-morrison-2000.github.io/source/chicagotree_dam.r'
 
-# Get data and helper functions from Richard's program
-if (run_local)
+ctree_local_file <- 'D:/CRTI/data/cleaned/dupage_county_accepted_V1.csv'
+ctree_http_url <- 'https://don-morrison-2000.github.io/data/dupage_county_accepted_V1.csv'
+
+# Source Richeard's modeling proram (either from local disk or the http server)
+if (file.exists(chicago_tree_app_local_file))
 {
-      source('D:/CRTI/r_projects/chicago_tree_dam/chicagotree_dam.r')
-      ctree <- readChicagoTree(path='D:/CRTI/data/', infile='cleaned/dupage_county_accepted_V1.csv')
-} else
+      source(chicago_tree_app_local_file)
+} else 
 {
-      source("https://don-morrison-2000.github.io/source/chicagotree_dam.r")
-      ctree <- readChicagoTree(infile='dupage_county_accepted_V1.csv', path='https://don-morrison-2000.github.io/data/')
+      source (chicago_tree_app_url)
 }
 
+# Read in the tree data (either from local disk or the http server)
+if (file.exists(ctree_local_file))
+{
+      ctree <- readChicagoTree(path='', infile=ctree_local_file)
+} else
+{
+      ctree <- readChicagoTree(path='', infile=ctree_http_url)
+}
+
+# These come from Richard's modeling program
 land_use <- landUse
 species <- c(commonSpecies$V1, "Other")
 
@@ -61,7 +73,7 @@ all_land_use <- unique(land_use$V1)
 # Model cache
 g_models <- list()
 # Ordered vector of plot predictors
-g_m_preds = character(0)
+g_m_preds <- character(0)
 
 # Models are maintined in a list with this format [[<<predictor formula]][[<species name]][[<land use desc>]]
 build_model <- function (ctree, predictors, species, land_use)
@@ -77,13 +89,13 @@ build_model <- function (ctree, predictors, species, land_use)
       {
             if (!is.null(land_use))
             {
-                  ctree <- subset(ctree, LU %in% land_use)
+                  ctree <- subset(ctree, as.numeric(LU) %in% land_use)
             }
-            if (nrow(ctree > 0))
+            if (nrow(ctree) > 0)
             {
                   ctree[,'occur'] <- 0
                   ctree[ctree[,'GENUSSPECI']==species,'occur'] <- 1
-                  model=glm(formula=as.formula(formula),family=binomial(link='logit'),data=ctree)
+                  model <- glm(formula=as.formula(formula),family=binomial(link='logit'),data=ctree)
                   model <- list(cf=summary(model)$coefficients,aic=summary(model)$aic, species=species, land_use=land_use_desc, sample_size=sum(ctree$occur))
                   # Cache the model in the global space - note the super-assign operator '<<-'
                   g_models[[formula]][[land_use_desc]][[species]] <<- model        
@@ -183,21 +195,22 @@ server <- function(input, output, session)
       # Ordered vector of spected model predictors
       m_preds_ordered <- reactiveValues(list = NULL)
       
-      # Update the plot predictors list when the model predictors are updated. The order of the plot predictor list reflects
+      # Update the plot predictors list when the model predictors are updated in the UI. The order of the plot predictor list reflects
       # the order that the predictors are listed in the model formula. 
       observeEvent (input$model_predictors, {
-            m_preds = character(0)
+            m_preds <- character(0)
             if (!is.null(input$model_predictors))
             {
-                  # Construct a list of potential plot predictors - in order that they were selected
-                  # Add existing choices (except those that have been deslected)
+                  # Construct a list of selected model predictors - in order that they were selected
+                  # First list in order the previously existing model predictors (except leave out those that have been deslected)
                   for (x in g_m_preds) {
                         if ((x %in% input$model_predictors)) 
                         {
                               m_preds <- c(m_preds, all_predictors[all_predictors==x])
                         }
                   }
-                  # Add in the newly selected predictors
+                  # Now add in the newly selected predictors. Since this is inside an observeEvent, we would expect only 
+                  # one new predictor. If there are more than one, then there is no way to determine the order - oh well....
                   for (x in input$model_predictors) {
                         if (!(x %in% m_preds)) 
                         {
@@ -205,9 +218,9 @@ server <- function(input, output, session)
                         }
                   }
             }
-            # Set the plot predictor list backed on the model predictors and update the UI
-            p_choices = m_preds
-            p_selected = if (input$plot_predictor %in% p_choices) input$plot_predictor else p_choices[1]
+            # Set the plot predictor choice to refect the ordered list of model predictors and update the UI
+            p_choices <- m_preds
+            p_selected <- if (input$plot_predictor %in% p_choices) input$plot_predictor else p_choices[1]
             updateRadioButtons(session, "plot_predictor", choices=as.list(m_preds), selected=p_selected)
             # Update the reactive value to propagate the new model predictors
             m_preds_ordered$list <- m_preds
@@ -299,9 +312,9 @@ server <- function(input, output, session)
       })
       
       output$outText <- renderTable({ 
-            col_names <- c('Species', 'Samples', 'Predictor','Estimate', 'Std. Error', 'z value', 'Pr(>|z|)')
+            col_names <- c('Species', 'Samples', 'Predictor','Estimate', 'Std. Error', 'z value', 'Pr(>|z|)', 'AIC')
             
-            stats <- data.frame(species=character(), sample_size=character(), predictor=character(), estimate=numeric(), std_error=numeric(), z_value=numeric(), pr=numeric())
+            stats <- data.frame(species=character(), sample_size=character(), predictor=character(), estimate=numeric(), std_error=numeric(), z_value=numeric(), pr=numeric(), aic=numeric())
             colnames(stats) <- col_names
             
             models <- get_models()
@@ -320,6 +333,7 @@ server <- function(input, output, session)
                         r[1,5] <- signif(m[2],4)
                         r[1,6] <- signif(m[3],4)
                         r[1,7] <- signif(m[4],4)
+                        r[1,8] <- signif(model$aic,4)
                         colnames(r) <- col_names
                         # Add the row to the table
                         stats <- rbind (stats, as.data.frame(r))
