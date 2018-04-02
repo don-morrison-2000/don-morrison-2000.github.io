0905e6641803aa4f7eb0f2b92d0c55c1c220a97f
don-morrison-2000 <35644326+don-morrison-2000@users.noreply.github.com>
2018-04-02 13:21:42 -0500

    Added missing library statements

--- source/shiny/species_by_landuse/app.R
+++ source/shiny/species_by_landuse/app.R
@@ -13,6 +13,8 @@
 
 library(shiny)
 library(ggplot2)
+library(reshape2)
+library(stringr)
 
 ctree_local_file <- 'D:/CRTI/data/cleaned/dupage_county_accepted_V4.csv'
 ctree_http_url <- 'https://don-morrison-2000.github.io/data/dupage_county_accepted_V4.csv'
